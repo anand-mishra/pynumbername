8c40c88ab9c601ea2a28a562c004cec742d7c0b4
anand <anand@outlook.cl>
2016-03-26 03:54:47 +0530

    fixed for pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
     name = "numbername",
     packages = ["numbername"],
-    version = "0.0.2",
+    version = "0.0.5",
     description = "Converts integers to number name",
     author = "Anand Mishra",
     author_email = "akm.inbox@hotmail.com",
     url = "https://github.com/anand-mishra/pynumbername",
-    download_url = "https://github.com/anand-mishra/pynumbername/raw/master/dist/numbername-0.0.1.tar.gz",
+    download_url =
+"https://pypi.python.org/packages/source/n/numbername/numbername-0.0.5.tar.gz",
     keywords = ["encoding", "i18n", "xml"],
     classifiers = [
         "Programming Language :: Python",
@@ -18,5 +19,37 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-    
+     long_description = """\
+Installation
+============
+
+pip install numbername
+
+
+Usage
+=====
+
+
+Convert integers to number name
+
+::
+
+    from numbername import to_number_name
+    print to_number_name(1123) 
+    #ouputs: one thousand one hundred and twenty three
+
+Convert integers to comma placed number
+
+::
+
+    from numbername import to_comma_placed
+    print to_number_name(1123)
+    #outputs: 1,123
+
+Limitations
+===========
+
+1. Works only with non-negative numbers
+2. Works only with integers
+"""
 )
